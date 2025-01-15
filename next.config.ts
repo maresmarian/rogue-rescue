4d4704c8915689a270a07e961e724c2867939999
Marián Mareš <marian.mares.021@pslib.cz>
2025-01-15 21:06:32 +0100

    snad

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,3 @@
-// next.config.ts
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     typescript: {
@@ -13,14 +12,24 @@ const nextConfig = {
     images: {
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     },
-    headers: async () => {
+    generateEtags: false,
+    unstable_runtimeJS: false,
+    async headers() {
         return [
             {
                 source: '/:path*',
                 headers: [
                     {
                         key: 'Cache-Control',
-                        value: 'no-store, max-age=0',
+                        value: 'no-cache, no-store, must-revalidate',
+                    },
+                    {
+                        key: 'Pragma',
+                        value: 'no-cache',
+                    },
+                    {
+                        key: 'Expires',
+                        value: '0',
                     },
                 ],
             },
