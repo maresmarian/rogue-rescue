2eab1b6106e5111835f9c4fca40bf449ac6ab390
Marián Mareš <marian.mares.021@pslib.cz>
2025-01-31 21:58:57 +0100

    jupiii

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { RootLayoutClient } from './RootLayoutClient';
 import AuthProvider from '@/providers/AuthProvider';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
     title: "Rogue Rescue Services",
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
     return (
         <AuthProvider>
+            <Analytics/>
             <RootLayoutClient>{children}</RootLayoutClient>
         </AuthProvider>
     );
